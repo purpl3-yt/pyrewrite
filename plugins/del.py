5f0837ae5de4df5d770c6a78191a4a6db59780f7
purpl3-yt <remezdevid2000@gmail.com>
2023-05-05 21:33:21 +0300

    fixed bugs

--- plugins/del.py
+++ plugins/del.py
@@ -5,9 +5,15 @@ from utils.help import help_menu
 
 @Client.on_message(filters.command('del', prefixes=prefix.get()) & filters.me)
 async def delete(client: Client, message: Message):
-    if message.outgoing:
+    if message.chat.id != message.from_user.id:
+        if message.outgoing:
+            if message.reply_to_message != None:
+                await message.reply_to_message.delete()
+            await message.delete()
+
+    else:
         if message.reply_to_message != None:
-            await message.reply_to_message.delete()
+                await message.reply_to_message.delete()
         await message.delete()
 
 help_menu.add_command('del', 'Deletes message', 'Deletes message that you reply')
