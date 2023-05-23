2a2e3ab101c86b00613d6c0df3138770c7988659
purpl3-yt <remezdevid2000@gmail.com>
2023-05-23 19:46:52 +0300

    fixed sets reset command

--- plugins/set.py
+++ plugins/set.py
@@ -52,7 +52,7 @@ async def sets(client: Client, message: Message):
         else:
             if args[1] == 'reset':
                 cmd_found.set_default_value()
-                await warn(message, 'Setting set to default value', 'done')
+                await warn(message, f'Setting {cmd_found.get_name()} to default value', 'done')
                 return
 
         if cmd_found == None:
